2546cc78cc1cc6d853b53899257448557ebad234
Anusha Ganti <27336160+anusha-66@users.noreply.github.com>
2018-04-01 23:45:14 -0400

    Update Exercise4.js

--- Exercise4.js
+++ Exercise4.js
@@ -1,12 +1,9 @@
-
-Step 2: Make changes to your code following the comments in the file. */
-
 <html>
 <body>
 
 <script type = "text/javascript">
 
-var sum = 3 + 8 + 8;
+var sum = 13 + 8 + 8;
 
 //Replace 8 with another number so that sum will be 29
 
@@ -24,13 +21,13 @@ var product = 11*6;
 
 document.write(product + "<br />");
 
-var quotient = 120/30;
+var quotient = 120/3;
 
 //Replace 10 with another number so that quotient will be 40
 
 document.write(quotient + "<br />");
 
-var remainder = 93%;
+var remainder = 93%12;
 
 //Replace 5 with another number so that remainder will be 9
 

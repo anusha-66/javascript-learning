06e860571cb8d28e29201ee65732c104daf25804
Anusha Ganti <27336160+anusha-66@users.noreply.github.com>
2018-04-01 21:43:10 -0400

    Create Exercise2.js

---
+++ Exercise2.js
@@ -0,0 +1,18 @@
+/* Collect the name of the user using prompt method. Store it in a variable. 
+Display the message "Hi name !!! Welcome to the course!!!" in an alert box. 
+The name should be replaced by the value entered by the user. */
+
+<html>
+<body>
+
+<script type = "text/javascript">
+
+var username = prompt("Name", "Please enter your name:" );
+alert(" Hi "+username+ "!!! Welcome to the course!!!");
+
+
+
+</script>
+
+</body>
+</html>

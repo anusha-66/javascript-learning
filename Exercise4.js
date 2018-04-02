dc606fd2ad82ab92fc9838deb728400f8b6cfe9e
Anusha Ganti <27336160+anusha-66@users.noreply.github.com>
2018-04-01 23:42:29 -0400

    Create Exercise4.js

---
+++ Exercise4.js
@@ -0,0 +1,41 @@
+
+Step 2: Make changes to your code following the comments in the file. */
+
+<html>
+<body>
+
+<script type = "text/javascript">
+
+var sum = 3 + 8 + 8;
+
+//Replace 8 with another number so that sum will be 29
+
+document.write(sum + "<br />");
+
+var difference =  34 - 25;
+
+//Replace 12 with another number so that difference will be 9
+
+document.write(difference + "<br />");
+
+var product = 11*6;
+
+//Replace 9 with another number so that product will be 66
+
+document.write(product + "<br />");
+
+var quotient = 120/30;
+
+//Replace 10 with another number so that quotient will be 40
+
+document.write(quotient + "<br />");
+
+var remainder = 93%;
+
+//Replace 5 with another number so that remainder will be 9
+
+document.write(remainder + "<br />");
+</script>
+
+</body>
+</html>

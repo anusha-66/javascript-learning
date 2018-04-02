dab82879423cbb0b9a825cb9a60dc29e468bd3f1
Anusha Ganti <27336160+anusha-66@users.noreply.github.com>
2018-04-01 21:37:27 -0400

    Create Exercise1.js
    Basic variables

---
+++ Exercise1.js
@@ -0,0 +1,54 @@
+/* Exercise:
+
+Step 1: Declare a variable named myName.
+
+Step 2: Declare a variable named myAge and assign your age to it.
+
+Step 3: Copy "alert(myName);" and paste in your code and see the output.
+
+Step 4: Declare a variable named isInterested and assign true to it.
+
+Step 5: Assign your name to myName variable.
+
+Step 6: Copy "alert(myName);" and paste in your code and see the output.
+
+
+___________________________________________________________________________________________________________ */
+
+<html>
+<body>
+
+<script type = "text/javascript">
+
+//Step 1:
+
+var myName = "";
+
+//Step 2:
+
+var myAge = 23;
+
+//Step 3:
+
+alert(myName);
+
+//Step 4:
+
+var isInterested = true;
+
+//step 5:
+
+myName = "Anusha";
+
+//Step 6:
+
+alert(myName);
+
+
+
+
+
+</script>
+
+</body>
+</html>
